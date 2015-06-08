11ccc836b07361fcd6e6099300c64e2336ee68e3
SleepingTalent <jaybono30@hotmail.com>
2015-06-08 06:26:01 +0100

    Added Role Validation and completed Login and Logout Feature and test

--- absence-tracker/src/main/java/com/noveria/absencemanagement/view/helper/MessageHelper.java
+++ absence-tracker/src/main/java/com/noveria/absencemanagement/view/helper/MessageHelper.java
@@ -3,13 +3,12 @@ package com.noveria.absencemanagement.view.helper;
 import org.springframework.stereotype.Component;
 
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
 /**
  * Created by lynseymcgregor on 08/06/2015.
  */
-@ManagedBean
+@Component
 public class MessageHelper {
 
     public void addErrorMessage(String summary, String detail) {
